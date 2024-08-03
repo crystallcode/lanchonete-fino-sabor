c23522aa74ae4d005bd4ca442f4c642f593b47cc
heitorffv <162719101+heitorffv@users.noreply.github.com>
2024-08-02 23:33:01 -0300

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,53 +1,54 @@
-const menu = document.getElementById("menu")
-const cartBtn = document.getElementById("cart-btn")
-const cartModal = document.getElementById("cart-modal")
-const cartItemsContainer = document.getElementById("cart-items")
-const cartTotal = document.getElementById("cart-total")
-const checkoutBtn = document.getElementById("checkout-btn")
-const closeModalBtn = document.getElementById("close-modal-btn")
-const cartCounter = document.getElementById("cart-count")
-const addressInput = document.getElementById("address")
-const addressWarn = document.getElementById("address-warn")
-const pontorefInput = document.getElementById('pontoref')
-const nameuserInput = document.getElementById('nameuser')
-const nameuserWarn = document.getElementById('nameuser-warn')
-const cashamountInput = document.getElementById('cash-amount')
-const observacoesInput = document.getElementById('observacoes')
-const trocoWarn = document.getElementById('troco-warn')
+const menu = document.getElementById("menu");
+const cartBtn = document.getElementById("cart-btn");
+const cartModal = document.getElementById("cart-modal");
+const cartItemsContainer = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const checkoutBtn = document.getElementById("checkout-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const cartCounter = document.getElementById("cart-count");
+const addressInput = document.getElementById("address");
+const addressWarn = document.getElementById("address-warn");
+const pontorefInput = document.getElementById('pontoref');
+const nameuserInput = document.getElementById('nameuser');
+const nameuserWarn = document.getElementById('nameuser-warn');
+const cashamountInput = document.getElementById('cash-amount');
+const observacoesInput = document.getElementById('observacoes');
+const trocoWarn = document.getElementById('troco-warn');
+const deliveryFields = document.getElementById('delivery-fields');
+const deliveryMethodRadios = document.querySelectorAll('input[name="delivery-method"]');
+const paymentMethodRadios = document.querySelectorAll('input[name="payment"]');
 
 let cart = [];
+let paymentMethod = '';
 
 // Abrir o modal do carrinho
 cartBtn.addEventListener("click", function() {
-    cartModal.style.display = "flex"
-})
+    cartModal.style.display = "flex";
+});
 
 // Fechar modal quando clicar fora
 cartModal.addEventListener("click", function(event){
     if(event.target === cartModal){
-        cartModal.style.display = "none"
+        cartModal.style.display = "none";
     }
-})
+});
 
 closeModalBtn.addEventListener("click", function(){
-    cartModal.style.display = "none"
-})
+    cartModal.style.display = "none";
+});
 
 menu.addEventListener("click", function(event){
-
-    let parentButton = event.target.closest(".add-to-cart-btn")
-
+    let parentButton = event.target.closest(".add-to-cart-btn");
     if(parentButton){
-        const name = parentButton.getAttribute("data-name")
-        const price = parseFloat(parentButton.getAttribute("data-price"))
-        addToCart(name, price)
+        const name = parentButton.getAttribute("data-name");
+        const price = parseFloat(parentButton.getAttribute("data-price"));
+        addToCart(name, price);
     }
-})
+});
 
 // Função para adicionar no carrinho
 function addToCart(name, price){
-    const existingItem = cart.find(item => item.name === name)
-
+    const existingItem = cart.find(item => item.name === name);
     if(existingItem){
         // Se o item já existe, aumenta apenas a quantidade +1
         existingItem.quantity += 1;
@@ -56,44 +57,37 @@ function addToCart(name, price){
             name,
             price,
             quantity: 1,
-        })
+        });
     }
-
-    updateCartModal()
+    updateCartModal();
 }
 
 // Atualiza o carrinho
 function updateCartModal(){
     cartItemsContainer.innerHTML = "";
     let total = 0;
-
     cart.forEach(item => {
         const cartItemElement = document.createElement("div");  
-        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
-
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col");
         cartItemElement.innerHTML = `
-        <div class="flex items-center justify-between">
-            <div>
-                <p class="font-medium">${item.name}</p>
-                <p>Quantidade: ${item.quantity}</p>
-                <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+            <div class="flex items-center justify-between">
+                <div>
+                    <p class="font-medium">${item.name}</p>
+                    <p>Quantidade: ${item.quantity}</p>
+                    <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+                </div>
+                <button class="remove-from-cart-btn" data-name="${item.name}">
+                    Remover
+                </button>
             </div>
-
-            <button class="remove-from-cart-btn" data-name="${item.name}">
-                Remover
-            </button>
-        </div>
-    `
-
+        `;
         total += item.price * item.quantity;
-        cartItemsContainer.appendChild(cartItemElement)
-    })
-
+        cartItemsContainer.appendChild(cartItemElement);
+    });
     cartTotal.textContent = total.toLocaleString("pt-BR", {
         style: "currency",
         currency: "BRL"
     });
-
     cartCounter.innerHTML = cart.length;
 }
 
@@ -107,10 +101,8 @@ cartItemsContainer.addEventListener("click", function(event) {
 
 function removeItemCart(name) {
     const index = cart.findIndex(item => item.name === name);
-
     if(index !== -1) {
         const item = cart[index];
-
         if(item.quantity > 1) {
             item.quantity -= 1;
         } else {
@@ -122,37 +114,58 @@ function removeItemCart(name) {
 
 nameuserInput.addEventListener("input", function(event){
     let inputValue = event.target.value;
-
     if(inputValue !== ""){
-        nameuserInput.classList.remove("border-red-500")
-        nameuserWarn.classList.add("hidden")
+        nameuserInput.classList.remove("border-red-500");
+        nameuserWarn.classList.add("hidden");
     }
-})
+});
 
 addressInput.addEventListener("input", function(event){
     let inputValue = event.target.value;
-
     if(inputValue !== ""){
-        addressInput.classList.remove("border-red-500")
-        addressWarn.classList.add("hidden")
+        addressInput.classList.remove("border-red-500");
+        addressWarn.classList.add("hidden");
     }
-})
+});
 
 cashamountInput.addEventListener("input", function(event){
     let inputValue = event.target.value;
-
     if(inputValue !== ""){
-        cashamountInput.classList.remove("border-red-500")
-        trocoWarn.classList.add("hidden")
+        cashamountInput.classList.remove("border-red-500");
+        trocoWarn.classList.add("hidden");
     }
-})
+});
+
+// Controle de método de entrega (Delivery ou Pickup)
+deliveryMethodRadios.forEach(input => {
+    input.addEventListener('change', function() {
+        const isDelivery = document.getElementById('delivery').checked;
+        if (isDelivery) {
+            deliveryFields.classList.remove('hidden');
+            addressWarn.classList.add('hidden');
+        } else {
+            deliveryFields.classList.add('hidden');
+            addressWarn.classList.add('hidden');
+        }
+    });
+});
+
+// Controle de método de pagamento
+paymentMethodRadios.forEach(input => {
+    input.addEventListener('change', function() {
+        paymentMethod = document.querySelector('input[name="payment"]:checked').value;
+        if (paymentMethod === 'cash') {
+            document.getElementById('change-selection').classList.remove('hidden');
+        } else {
+            document.getElementById('change-selection').classList.add('hidden');
+        }
+    });
+});
 
 // Finalizar pedidos
 checkoutBtn.addEventListener("click", function(){
-
     const isOpen = checkRestaurantOpen();
     if(!isOpen){
-
         Toastify({
             text: "Ops, o restaurante está fechado no momento",
             duration: 3000,
@@ -164,28 +177,25 @@ checkoutBtn.addEventListener("click", function(){
                 background: "#ef4444",
             },
         }).showToast();
-
         return;
     }
 
     if(cart.length === 0) return;
     if(nameuserInput.value === ""){
-        nameuserWarn.classList.remove("hidden")
-        nameuserWarn.classList.add("border-red-500")
+        nameuserWarn.classList.remove("hidden");
+        nameuserWarn.classList.add("border-red-500");
         return;
     }
 
-    if(cart.length === 0) return;
-    if(addressInput.value === ""){
-        addressWarn.classList.remove("hidden")
-        addressWarn.classList.add("border-red-500")
+    if(document.getElementById('delivery').checked && addressInput.value === ""){
+        addressWarn.classList.remove("hidden");
+        addressWarn.classList.add("border-red-500");
         return;
     }
 
-    if(cart.length === 0) return;
-    if(cashamountInput.value === ""){
-        trocoWarn.classList.remove("hidden")
-        trocoWarn.classList.add("border-red-500")
+    if(paymentMethod === 'cash' && cashamountInput.value === ""){
+        trocoWarn.classList.remove("hidden");
+        trocoWarn.classList.add("border-red-500");
         return;
     }
 
@@ -196,28 +206,29 @@ checkoutBtn.addEventListener("click", function(){
         );
     }).join("\n");
 
+    const deliveryMethod = document.getElementById('delivery').checked ? "Delivery" : "Retirar no local";
+    const paymentInfo = paymentMethod === 'cash' ? `Dinheiro, precisa de troco: ${cashamountInput.value}` : "Pix";
+
+
     const message = encodeURIComponent(`
 ${cartItems}
 
 Nome do cliente: ${nameuserInput.value}
 
-Endereço: ${addressInput.value}
+Método de retirada: ${deliveryMethod}
+${deliveryMethod === "Delivery" ? `Endereço: ${addressInput.value}\nPonto de referência: ${pontorefInput.value}\n` : ''}
 
-Ponto de referência: ${pontorefInput.value}
-
-O cliente precisa de troco? ${cashamountInput.value}
+Forma de pagamento: ${paymentInfo}
 
 Observações do cliente, caso tenha: '${observacoesInput.value}'
-
     `);
 
     const phone = "+5579996041990";
-
     window.open(`https://wa.me/${phone}?text=${message}`, "_blank");
 
     cart = [];
     updateCartModal();
-})
+});
 
 function checkRestaurantOpen(){
     const data = new Date();
@@ -225,13 +236,13 @@ function checkRestaurantOpen(){
     return hora >= 18 && hora < 23; // true
 }
 
-const spanItem = document.getElementById("date-span")
+const spanItem = document.getElementById("date-span");
 const isOpen = checkRestaurantOpen();
-
 if(isOpen){
     spanItem.classList.remove("bg-red-500");
-    spanItem.classList.add("bg-green-600")
+    spanItem.classList.add("bg-green-600");
 } else {
-    spanItem.classList.remove("bg-green-600")
-    spanItem.classList.add("bg-red-500")
+    spanItem.classList.remove("bg-green-600");
+    spanItem.classList.add("bg-red-500");
 }
+
