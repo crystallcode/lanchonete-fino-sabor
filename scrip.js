cf63521e8a3ba7196f4f38c7c6fb453605b77df2
heitorffv <162719101+heitorffv@users.noreply.github.com>
2024-08-10 12:01:12 -0300

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -233,7 +233,7 @@ Observações do cliente, caso tenha: '${observacoesInput.value}'
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18;30 && hora < 23;01; // true
+    return hora >= 18 && hora < 23;01; // true
 }
 
 const spanItem = document.getElementById("date-span");
